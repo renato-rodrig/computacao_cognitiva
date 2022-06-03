ffd7b67408bf250e03ac689e50a444a6415bcba9
renato-rodrig <rodriguesrenato100@gmail.com>
2022-06-02 21:15:33 -0300

    atividade de sala PDI

---
+++ ativ_sala/PDI/Aula_1/code3.py
@@ -0,0 +1,23 @@
+from PDI.src.pdi_utils import load_lena
+import matplotlib.pyplot as plt
+
+from PDI.src.pdi_utils import load_red_roses,show_image
+import matplotlib.pyplot as plt
+
+image = load_red_roses()
+
+# Show original image
+show_image(image,'image RGB')
+
+# Obtain the red channel
+red_channel = image[:,:,0]
+
+# Show original image
+show_image(red_channel,'image red channel')
+
+# Plot the red histogram with bins in a range of 256
+plt.hist(red_channel, bins=256)
+
+# Set title and show
+plt.title('Red Histogram')
+plt.show()
